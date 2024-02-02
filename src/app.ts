19b991e023bdf4624ad49e7e66f348cea91ff39b
intellect-sam <alukoolakunlesamuel@gmail.com>
2024-02-02 16:41:37 +0100

    connecting with db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { dbConnection } from './config/dbConnect';
+import connectDB from './config/dbConnect';
+
 const app = express();
+
+const PORT = process.env.PORT || 3500;
+
+connectDB();
 
 app.get('/', (req, res) => {
   return res.send('hello world');
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
 });
+
+// process.on('SIGINT', () => {
+//   dbConnection.close((err) => {
+//     if (err) {
+//       console.log('Error occurred while closing MongoDB connection', err);
+//       return false;
+//     } else {
+//       console.log('App terminated successfully');
+//       return true;
+//     }
+//     process.exit(0);
+//   });
+// });
---
+++ src/config/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose, { Connection } from 'mongoose';
+
+const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL as string);
+
+    console.log('Connected to MongoDB');
+
+    mongoose.connection.on('connected', () =>
+      console.log('Mongoose connected to MongoDB')
+    );
+    mongoose.connection.on('error', (err) =>
+      console.log(`Mongoose connection error: ${err}`)
+    );
+    mongoose.connection.on('disconnected', () =>
+      console.log('Mongoose disconnected')
+    );
+  } catch (err) {
+    console.error('Error connecting to MongoDB: ', err);
+    process.exit(1);
+  }
+};
+
+const dbConnection: Connection = mongoose.connection;
+
+export default connectDB;
+export { dbConnection };
